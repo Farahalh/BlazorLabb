9ce9f621691a9275a774c72e92da355a7f82768d
Farah Alhaddad <farah.alhaddad@hotmail.com>
2024-10-12 16:28:16 +0200

    Moving code to code behind and adding method to view message @onclick.

---
+++ BlazorLabb/Components/Pages/RegisterUser.razor.cs
@@ -0,0 +1,12 @@
+namespace BlazorLabb.Components.Pages
+{
+    public partial class RegisterUser
+    {
+        private string userMessage = "";
+
+        private void RegisterNewUser()
+        {
+            userMessage = "User registration was successful!";
+        }
+    }
+}
