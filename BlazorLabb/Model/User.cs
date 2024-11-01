0b4220651b14bc2bde5d6a48834ce593a565f964
Farah Alhaddad <farah.alhaddad@hotmail.com>
2024-11-01 09:46:42 +0100

    - Added ctor to define the functionality of the class User. - Added a summary to describe the purpose of the class User.

--- BlazorLabb/Model/User.cs
+++ BlazorLabb/Model/User.cs
@@ -1,15 +1,28 @@
 ﻿namespace BlazorLabb.Model
 {
+    /// <summary>
+    /// Represents a data model for user information, serving as a container for user-related properties.
+    /// This class is solely for holding data and contains no functional behavior.
+    /// </summary>
     public class User
     {
-        // TODO: This structure feels wrong, how can I make sure that the classes are connected?
-        // Do I need this nested class or can I open it up? Or can I use existing classes for PErson, Company, Address.
         public int Id { get; set; }
         public string? Name { get; set; }
         public string? UserName { get; set; }
         public string? Email { get; set; }
         public string? Password { get; set; }
-        public Company? company { get; set; }
-        public Address? streetName { get; set; }
+        public Company? Company { get; set; }
+        public Address? Address { get; set; }
+
+        public User(int id, string name, string userName, string email, string password, Company company, Address address)
+        {
+            Id = id;
+            Name = name;
+            UserName = userName;
+            Email = email;
+            Password = password;
+            Company = company;
+            Address = address;
+        }
     }
 }
