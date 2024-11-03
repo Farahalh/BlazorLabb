f303a8798e48aa6034c86cc7912ede3b8133660a
Farah Alhaddad <farah.alhaddad@hotmail.com>
2024-11-03 17:21:33 +0100

    Add XML Documentation to Company Class
    - Added XML summary documentation to the Company class.
    - Clarified the purpose and usage of the Company class and its properties.

--- BlazorLabb/Model/Company.cs
+++ BlazorLabb/Model/Company.cs
@@ -1,4 +1,9 @@
-﻿namespace BlazorLabb.Model
+﻿/// <summary>
+/// Represents a company with a name and a catchphrase.
+/// This class is used to store information about a company that may be associated with a user or other entities.
+/// </summary>
+
+namespace BlazorLabb.Model
 {
     public class Company
     {
