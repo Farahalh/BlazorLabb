7da62c58f41c468ae4ea93207e89bf9e13d9375e
Farah Alhaddad <Farah.alhaddad@hotmail.com>
2024-11-26 23:49:55 +0100

    Add UserDataAccessCreator to instantiate IUserDataAccess based on DataSource.

---
+++ BlazorLabb/Model/UserDataAccessCreator.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorLabb.Model
+{
+    public class UserDataAccessCreator
+    {
+        public static IUserDataAccess Create(DataSource dataSource, int userCount)
+        {
+            return dataSource switch
+            {
+                DataSource.Random => new UserDataAccess(userCount),
+                DataSource.Json => new JsonUserDataAccess(),
+                DataSource.API => new APIUserDataAccess(userCount),
+                _ => throw new ArgumentException("Argument Not Valid", nameof(dataSource)),
+            };
+        }
+    }
+}
